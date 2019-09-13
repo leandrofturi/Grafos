2ec219089b9e224c2bfdc95e1bfcd9b81181f96e
Leandro Furlam <leandrofturi@gmail.com>
2019-09-13 14:40:01 -0300

    inicializacao

--- grafo.cpp
+++ grafo.cpp
@@ -2,32 +2,25 @@
 
 grafo::grafo() {}
 
-grafo::~grafo()
-{
-    for(size_t i = 0; i < E.size(); i++)
-    {
-        delete E[i];
-    }
-}
-
 void grafo::addV(int v)
 {
     this->V.push_back(v);
 }
 
-void grafo::addE(int e[2])
+void grafo::addE(int v1, int v2)
 {
+    int e[2];
+    e[0] = v1;
+    e[1] = v2;
     this->E.push_back(e);
 }
 
-void grafo::setMatrizAdj(std::vector<std::vector<int>> matrizAdj)
+void grafo::setMatrizAdj(std::vector<std::vector<int> > matrizAdj)
 {
     this->matrizAdj = matrizAdj;
 }
 
-/*
-void grafo::setListaInc(std::vector<std::vector<int> listaInc)
+void grafo::setListaInc(std::vector<std::list<int> > listaInc)
 {
     this->listaInc = listaInc;
 }
-*/
--- grafo.hpp
+++ grafo.hpp
@@ -3,22 +3,23 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 class grafo
 {
 public:
     std::vector<int> V;
     std::vector<int*> E;
-    std::vector<std::vector<int>> matrizAdj;
-    //std::vector<int*> listaInc;
+    std::vector<std::vector<int> > matrizAdj;
+    std::vector<std::list<int> > listaInc;
 
     grafo();
     ~grafo();
 
     void addV(int v);
-    void addE(int e[2]);
-    void setMatrizAdj(std::vector<std::vector<int>> matrizAdj);
-    //void setListaInc(std::vector<int*> listaInc);
+    void addE(int v1, int v2);
+    void setMatrizAdj(std::vector<std::vector<int> > matrizAdj);
+    void setListaInc(std::vector<std::list<int> > listaInc);
 };
 
 #endif
