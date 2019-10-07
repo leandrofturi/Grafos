d3dd6c515b58d8f434c9847c9052a58e388b757d
Leandro Furlam <leandrofturi@gmail.com>
2019-10-06 21:36:36 -0300

    raiva

--- componentesConexas.cpp
+++ componentesConexas.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <vector>
 
 #define SWAP(a,b) a^=b^=a^=b;
 
 int main()
 {
-    int N = 1, V, E, cont;
+    int N, V, E, cont;
     char a, b;
 
     std::cin >> N;
@@ -13,35 +12,51 @@ int main()
     {
         cont = 0;
         std::cin >> V >> E;
-        std::vector<int> adj[V];
-        int aux[V];
-        
-        for(size_t j = 0; j < E; j++)
+        int aux[V] = {0};
+        for (size_t j = 0; j < E; j++)
         {
             std::cin >> a >> b;
-            if(a > b)
-                SWAP(a,b);
-            adj[(a-'a')].push_back((b-'a'));
-            aux[(b-'a')] = 1;
-        }
-        std::cout << "Case #" << i+1 << ":" << std::endl;
-        for(size_t k = 0; k < V; k++)
-        {
-            if(aux[k] != 1)
+            if((aux[a-'a'] == 0) && (aux[b-'a'] == 0))
             {
                 cont ++;
-                std::cout << (char) (k+'a') << ",";
-            
-                for(size_t l = 0; l < adj[k].size(); l++)
+                aux[a-'a'] = aux[b-'a'] = cont;
+            }
+            else if(aux[a-'a'] == 0)
+            {
+                aux[a-'a'] = aux[b-'a'];
+            }
+            else
+            {
+                aux[b-'a'] = aux[a-'a'];
+            }
+        }
+
+        std::cout << "Case #" << i+1 << ":" << std::endl;
+        for (size_t j = 0; j < V; j++)
+        {
+            if(aux[j] == 0)
+            {
+                cont ++;
+                std::cout << (char) (j + 'a') << ",";
+                std::cout << std::endl;
+            }
+            else if(aux[j] != -1)
+            {
+                std::cout << (char) (j + 'a') << ",";
+                for (size_t k = j+1; k < V; k++)
                 {
-                    std::cout << (char) (adj[k][l]+'a') << ",";
+                    if((aux[j] == aux[k]) && (aux[k] != -1))
+                    {
+                        std::cout << (char) (k + 'a') << ",";
+                        aux[k] = -1;
+                    }
                 }
                 std::cout << std::endl;
             }
+            aux[j] == -1;
         }
-        std::cout << cont << " connected components" << std::endl;
-        std::cout << std::endl;
+        std::cout << cont << " connected components" << std::endl << std::endl;
     }
-    
+    std::cout << std::endl;
     return 0;
 }
