402caf4ad7a9df17ddeff0ee080147e9ce9fd128
Leandro Furlam <leandrofturi@gmail.com>
2019-11-28 19:55:17 -0300

    trab2

--- ArvoreGeradora.cpp
+++ ArvoreGeradora.cpp
@@ -5,50 +5,57 @@
 
 int main() {
     int N, M, U, V, C;
-    scanf(" %d %d", &N, &M);
+    std::cin >> N >> M;
 
     int VLinha[N] = {0};
-    int L[N] = {0};
+    int L[N];
     //Acesso a matriz: j + N*(i)
     int adj[N*N] = {0};
     for(int i = 0; i < M; i++) {
-        scanf(" %d %d %d", &U, &V, &C);
+        std::cin >> U >> V >> C;
         adj[(V-1) + N*(U-1)] = adj[(U-1) + N*(V-1)] = C;
     }
 
     int total = 0;
 
-    int w = 0, u = 0;
+    int w, u = 0;
     int min;
     VLinha[0] = 1;
-    for(int i = 0; i < N; i++)
-        L[i] = adj[i];
+    for(int i = 0; i < N; i++) {
+        L[i] = (adj[i] != 0) ? adj[i] : INF;
+    }
 
-    for(int faltam = N-1; faltam > 0; faltam--) {
+    for(int faltam = N; faltam > 0; faltam--) {
         min = INF;
         for(int i = 0; i < N; i++) {
-            if(!VLinha[i] && !adj[u + N*i]) {
+            if(!VLinha[i]) {
                 min = MIN(min, L[i]);
-                w = (min == L[i]) ? w : i;
+                w = (min == L[i]) ? i : w;
+            }
+        }
+        if(L[w] == INF)
+            break;
+
+        min = INF;
+        for(int i = 1; i < N; i++) {
+            if(VLinha[i] && adj[i + N*w]) {
+                min = MIN(min, adj[w + N*i]);
+                u = (min == adj[w + N*i]) ? i : u;
             }
         }
         VLinha[w] = 1;
 
-        min = INF;
-        for(int i = 0; i < N; i++) {
-            if(!VLinha[i] && !adj[i + N*w]) {
-                min = MIN(min, adj[w + N*i]);
-                u = (min == adj[w + N*i]) ? u : i;
-            }
-        }
-
         total += adj[u + N*w];
 
         for(int i = 0; i < N; i++) {
-            if(!VLinha[i] && (adj[i + N*w] < L[i]) && !adj[i + N*w])
-                L[i] = adj[i + N*w];
+            if(!VLinha[i])
+                if((L[i] > adj[i + N*w]) && adj[i + N*w])
+                    L[i] = adj[i + N*w];
         }
+        w = 0;
     }
 
-    printf("%d\n", total);
+    std::cout << total << std::endl;
+
+    return 0;
 }
