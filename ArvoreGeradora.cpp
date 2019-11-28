c8529316c835f3e0911b2ed3d1da0197648f821d
Leandro Furlam Turi <leandrofturi@gmail.com>
2019-11-28 18:19:46 -0300

    trab 2

--- ArvoreGeradora.cpp
+++ ArvoreGeradora.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
 
-int INF = 1073741823;
-
-int fixo[500];
-int custo[500];
+#define INF 1073741823
+#define MIN(x,y) (x < y) ? x : y
 
 int main() {
     int N, M, U, V, C;
     scanf(" %d %d", &N, &M);
 
-    int adj[N][N] = {0};
+    int VLinha[N] = {0};
+    int L[N] = {0};
+    //Acesso a matriz: j + N*(i)
+    int adj[N*N] = {0};
     for(int i = 0; i < M; i++) {
         scanf(" %d %d %d", &U, &V, &C);
-        adj[U-1][V-1] = adj[V-1][U-1] = C;
-    }
-
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            printf("%d ", adj[i][j]);
-        }
-        printf("\n");
+        adj[(V-1) + N*(U-1)] = adj[(U-1) + N*(V-1)] = C;
     }
 
     int total = 0;
 
-    for(int i=0; i<N; i++) {
-        fixo[i] = 0;
-        custo[i] = INF;
+    int w = 0, u = 0;
+    int min;
+    VLinha[0] = 1;
+    for(int i = 0; i < N; i++)
+        L[i] = adj[i];
+
+    for(int faltam = N-1; faltam > 0; faltam--) {
+        min = INF;
+        for(int i = 0; i < N; i++) {
+            if(!VLinha[i] && !adj[u + N*i]) {
+                min = MIN(min, L[i]);
+                w = (min == L[i]) ? w : i;
+            }
+        }
+        VLinha[w] = 1;
+
+        min = INF;
+        for(int i = 0; i < N; i++) {
+            if(!VLinha[i] && !adj[i + N*w]) {
+                min = MIN(min, adj[w + N*i]);
+                u = (min == adj[w + N*i]) ? u : i;
+            }
+        }
+
+        total += adj[u + N*w];
+
+        for(int i = 0; i < N; i++) {
+            if(!VLinha[i] && (adj[i + N*w] < L[i]) && !adj[i + N*w])
+                L[i] = adj[i + N*w];
+        }
     }
-    custo[0] = 0;
-
-    for(int faltam = N; faltam > 0; faltam --) {
-        int no = -1;
-        for(int i = 0; i < N; i ++)
-            if(!fixo[i] && (no == -1 || custo[i] < custo[no]))
-                no = i;
-        fixo[no] = 1;
-
-        if(custo[no] == INF) {
-            total = INF;
-            break;
-        }
-        total += custo[no];
-
-        for(int i=0; i<N; i++)
-            if(custo[i] > adj[no][i])
-            custo[i] = adj[no][i];
-        }
 
     printf("%d\n", total);
 }
